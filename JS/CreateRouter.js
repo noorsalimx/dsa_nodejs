d21428aca25eb42ad422b384bad31eceecf06e75
NSG <24814579+Blaade-Runner@users.noreply.github.com>
2020-01-22 17:04:13 +0530

    Update CreateRouter.js

--- JS/CreateRouter.js
+++ JS/CreateRouter.js
@@ -2,25 +2,20 @@ const http = require('http');
 
 let server = http.createServer(function (req, res) {   //create web server
     if (req.url == '/hi') { //check the URL of the current request
-
         // set response header
         res.writeHead(200, { 'Content-Type': 'text/html' });
 
         // set response content    
         res.write(`Url is 'http://127.0.0.1:8080/hi'`);
         res.end();
-
     }
     else if (req.url == "/hello") {
-
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(`Url is 'http://127.0.0.1:8080/hello'`);
         res.end();
-
     }
     else
         res.end('Invalid Request!');
-
 });
 
 server.listen(8080, '127.0.0.1');
