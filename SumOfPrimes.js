99f4184ebcd61978754fe95beee41008a60a80b7
NSG <24814579+Blaade-Runner@users.noreply.github.com>
2020-01-22 15:18:02 +0530

    Create SumOfPrimes.js

---
+++ SumOfPrimes.js
@@ -0,0 +1,20 @@
+function isPrime( n ) {
+    let max = Math.sqrt(n);
+    for( let i = 2;  i <= max;  i++ ) {
+        if( n % i === 0 )
+            return false;
+    }
+    return true;
+}
+
+function sum( limit ) {
+    let sum = 0;
+    for( let i = 2; i < limit; i++) {
+        if(isPrime(i)) {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+console.log(sum(2000000));
