639f0d1e313fdf69cc10ae67015c0839c7b20977
NSG <24814579+Blaade-Runner@users.noreply.github.com>
2020-02-11 11:35:35 +0530

    Create addTwoNumber_readline.js

---
+++ JS/addTwoNumber_readline.js
@@ -0,0 +1,16 @@
+/* add two number using readline core module*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please enter the first number : ', (answer1) => {
+    rl.question('Please enter the second number : ', (answer2) => {
+        var result = (+answer1) + (+answer2);
+        console.log(`The sum of above two numbers is ${result}`);
+        rl.close();
+    });
+});
