a407ba330b5803b348c259660614daf8d533e018
Mr.Lazy <24814579+mrlazygoku@users.noreply.github.com>
2020-05-17 04:58:48 +0530

    server

--- Node.JS/HandlingRequests/routers.js
+++ Node.JS/HandlingRequests/routers.js
@@ -1,5 +1,5 @@
 function route(path) {
-  console.log("From router. Path : ", path);
+  console.log("From router. Request received for : ", path);
 }
 
 module.exports = { route };
--- Node.JS/HandlingRequests/server.js
+++ Node.JS/HandlingRequests/server.js
@@ -1,14 +1,13 @@
 const http = require("http");
 const url = require("url");
-
-const routers = require("./routers");
 const port = process.env.PORT || 3000;
 
 function startServer(route) {
   const server = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("Application started from app module. ");
-    res.write(`Pathname : ${url.parse(req.url).pathname}`);
+    console.log(`Request received for : ${url.parse(req.url).pathname}`);
+    res.write(`Request received for : ${url.parse(req.url).pathname}`);
     res.end();
   });
 
