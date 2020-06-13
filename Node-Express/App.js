da72651b2397eb4ba33ba2f1199b6615943f4b75
Mr.Lazy <24814579+mrlazygoku@users.noreply.github.com>
2020-06-13 18:32:36 +0530

    m-ware set up

--- Node-Express/App.js
+++ Node-Express/App.js
@@ -2,22 +2,28 @@ const express = require("express");
 const app = express();
 
 // Middleware
+/* To serve static files such as images, CSS files, 
+  and JavaScript files, use the express.static built-in middleware function in Express.
+ */
+app.use(express.static("public")); // includes public dir
+
 app.use((req, res, next) => {
   console.log("Timestamp :", new Date().toLocaleString());
   next();
-});
-
-app.get("/", (req, res) => {
-  res.send("Hey!");
-});
-app.get("/send", (req, res) => {
-  res.sendFile(__dirname + "/SendingFile/index.html");
 });
 
 // Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something happened wrong!");
+});
+
+// Router
+app.get("/", (req, res) => {
+  res.send("Hey!");
+});
+app.get("/send", (req, res) => {
+  res.sendFile(__dirname + "/SendingFile/index.html");
 });
 
 const PORT = 3000;
